81cfd623372909ee72cead73f55bae553303fe3f
Alexey <beetrave@me.com>
2021-01-11 18:46:56 +0300

    add lesson #1 solution

---
+++ social network client - test_app/social network client - test_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  social network client - test_app
+//
+//  Created by Алексей Кодинцев on 11.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
